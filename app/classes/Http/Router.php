5ba10c312f6e1f1dcf65c4ee25fdf1512c8b2937
ANTIBIOTICS <abx@abx.pe.kr>
2023-05-14 00:40:10 +0900

    불필요한 상수 정의 제거

--- app/classes/Http/Router.php
+++ app/classes/Http/Router.php
@@ -9,33 +9,32 @@ use ContestApp\Controller\{Index, Account, Contest, Assets};
 class Router {
 
 	private \Bramus\Router\Router $router;
-	
-	private Array $serverParams      = [];
-	private Array $getParams         = [];
-	private Array $postParams        = [];
-	private Array $cookieParams      = [];
-	private Array $uploadFileParams  = [];
+
+	private Array $serverParams;
+	private Array $getParams;
+	private Array $postParams;
+	private Array $cookieParams;
+	private Array $uploadFileParams;
 
 	// 요청을 제어할 모든 경로를 설정
 	private function routing(): void {
-	
-		$router = &$this->router;
-	
-		$router->get(self::PATH_ROBOTS_TXT,      [Index::class, "robots"]);
-		$router->get(self::PATH_FAVICON_IMAGE,   [Index::class, "favicon"]);
-		
-		$router->get(self::PATH_ROOT,            [Index::class, "redirect"]);
-		$router->get(self::PATH_INDEX_PAGE,      [Index::class, "redirect"]);
-		$router->get(self::PATH_HOME_PAGE,       [Index::class, "home"]);
-		
-		$router->get(self::PATH_ASSETS_CONTENTS, [Assets::class, "assets"]);
 
-		$router->all(".*",                       [Index::class, "index"]);
-	
+		$this->router->get("/robots.txt",         [Index::class, "robots"]);
+		$this->router->get("/favicon.ico",        [Index::class, "favicon"]);
+
+		$this->router->get("/",                   [Index::class, "redirect"]);
+		$this->router->get("/index([^/]+)",       [Index::class, "redirect"]);
+		$this->router->get("/home",               [Index::class, "home"]);
+
+		$this->router->get("/assets/(.*)",        [Assets::class, "assets"]);
+
+		$this->router->all(".*",                  [Index::class, "index"]);
+
 	}
 
 	public function __construct() {
 		$this->router = new \Bramus\Router\Router();
+		$this->setParams([], [], [], [], []);
 	}
 
 	// 라우터에 사전정의 변수를 설정
@@ -46,26 +45,17 @@ class Router {
 		Array $cookieParams     = [],
 		Array $uploadFileParams = [],
 	): void {
-
 		$this->serverParams     = $serverParams;
 		$this->getParams        = $getParams;
 		$this->postParams       = $postParams;
 		$this->cookieParams     = $cookieParams;
-		$this->uploadFileParams = $uploadFileParams;	
-	
+		$this->uploadFileParams = $uploadFileParams;
 	}
 
-	// 요청을 라우터에 할당 
+	// 요청을 라우터에 할당
 	public function routeRequest(): void {
 		$this->routing();
 		$this->router->run();
 	}
-
-	public const PATH_ROBOTS_TXT      = "/robots.txt";
-	public const PATH_FAVICON_IMAGE   = "/favicon.ico";
-	public const PATH_ROOT            = "/";
-	public const PATH_INDEX_PAGE      = "/index([^/]+)";
-	public const PATH_HOME_PAGE       = "/home";
-	public const PATH_ASSETS_CONTENTS = "/assets/(.*)";
 
 };
