a7d7a20095b9ac65cac39d4d40ee943a99861b4a
ANTIBIOTICS <abx@abx.pe.kr>
2023-05-15 23:27:39 +0900

    PdoConnector 수정

--- app/classes/Database/PdoConnector.php
+++ app/classes/Database/PdoConnector.php
@@ -1,92 +1,66 @@
 <?php
 
 namespace ContestApp\Database;
-use \PDO, \PDOException;
 
-/**
- * MySQL DB 접속을 위한 PDO 객체와 관련 메소드를 정의 
- */
+use PDO, PDOException;
+
 class PdoConnector {
 
-	private ?PDO    $pdo;
-	private String  $pdoExceptionMessage;
+  public ?PDO $pdo;
 
-	private const PDO_OPTIONS = [
-		PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4",
-		PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-		PDO::ATTR_EMULATE_PREPARES   => false
-	];
+  private Array $options = [
+    PDO::MYSQL_ATTR_INIT_COMMAND       => "SET NAMES utf8mb4",
+    PDO::ATTR_ERRMODE                  => PDO::ERRMODE_EXCEPTION,      // 오류 발생시에는 PDOException 발생
+    PDO::ATTR_EMULATE_PREPARES         => false,                       //
+    PDO::ATTR_AUTOCOMMIT               => true,                        //
+    PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true,                        //
+    PDO::ATTR_DEFAULT_FETCH_MODE       => PDO::FETCH_ASSOC             //
+  ];
 
-	public function __construct() {
+  public function __construct(String $host, String $dbname, String $username, String $password) {
+    $this->init($host, $dbname, $username, $password);
+  }
 
-		$this->pdo = new PDO(
-			sprintf("mysql:host=%s;dbname=%s;charset=utf8", \MYSQL_HOSTNAME, \MYSQL_DBNAME),
-			MYSQL_USERNAME,
-			MYSQL_PASSWORD,
-			self::PDO_OPTIONS
-		);
-		$this->pdoExceptionMessage = "";
+  public function init(String $host, String $dbname, String $username, String $password): bool {
+    $dsn = sprintf("mysql:host=%s;dbname=%s;charset=utf8", $host, $dbname);
+    try {
+      $this->pdo = new PDO($dsn, $username, $password, $this->options);
+      return ($this->pdo !== null);
+    } catch (PDOException $e) {
+      return false;
+    }
+  }
 
-	}
+  public function __destruct() {
+    $this->pdo = null;
+  }
 
-	public function __destruct() {
-		$this->pdo = null;
-	}
+  public function insert(String $table, Array $values): int {
 
-	public function getPdoExceptionMessage(): String {
-		return $this->pdoExceptionMessage;
-	}
+    $params = "";
+    for ($i = 0; $i < count($values); $i++) {
+      $value = (strcmp(gettype($values[$i]), "integer") == 0) ?
+        $values[$i] : sprintf("'%s'", $values[$i]);
+      $params = sprintf("%s%s %s", $params, (($i > 0) ? "," : ""), $value);
+    }
 
-	public function insert(String $tableName, Array $values): int {
+    $query = sprintf("INSERT INTO %s VALUES(%s);", $table, $params);
 
-		$params = "";	
-		for ($i = 0; $i < count($values); $i++) {
-			$value = (strcmp(gettype($values[$i]), "integer") == 0) ?
-				$values[$i] : sprintf("'%s'", $values[$i]);
-			$params = sprintf("%s%s %s", $params, (($i > 0) ? "," : ""), $value);
-		}
+    return $this->submit($query);
 
-		$query = sprintf("INSERT INTO %s VALUES(%s);", $tableName, $params);
-		return $this->submit($query);
+  }
 
-	}
+  public function submit(String $query): int {
 
-	public function submit(String $query): bool {
-	
-		try {
-			$stmt = $this->pdo->prepare(trim($query));
-			$stmt->execute();
-			return true;
-		} catch (PDOException $e) {
-			$this->pdoExceptionMessage = $e->getMessage();
-			return false;
-		}
+    try {
+      $stmt = $this->pdo->prepare($query);
+      $stmt->execute();
+      return 1;
+    } catch (PDOException $e) {
+      return 0;
+    }
 
-	}
-
-	public function read(String $tableName, Array $conditions): Array {
-
-		$query = sprintf("SELECT * FROM %s WHERE %s='%s';", $tableName, 
-
-
-	public function delete(String $tableName, Array $conditions): int {
-
-		$query = sprintf("DELETE FROM %s", $tableName);
-		$conditionsCount = count($conditions);
-		
-		if ($conditionsCount > 0) {
-			$query = sprintf("%s WHERE", $query);
-		}
-		for ($i = 0; $i < $conditionsCount; $i++) {
-			
-		
-			$query = sprintf("%s %s='%s'", $query, $conditions[$i][0], $conditions[$i][1]);
-			if ($i != $conditionsCount - 1) {
-				$query = sprintf("%s AND "
-			}
-		}
-
-	}
+  }
 
   public function read(String $table, Array $conditions): Array {
 
@@ -109,4 +83,5 @@ class PdoConnector {
     return $this->submit($query);
 
   }
+
 };
