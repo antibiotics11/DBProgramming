bd001cfa6fe028d2a08648baad50f2c66ee04432
ANTIBIOTICS <abx@abx.pe.kr>
2023-05-19 18:15:00 +0900

    변수명 변경, 쿼리 값을 base64에서 url 인코딩으로 수정

--- app/assets/script/xhttp.js
+++ app/assets/script/xhttp.js
@@ -1,16 +1,16 @@
 /**
  * 서버로 비동기 POST 요청을 보낸다
  *
- * @param {string}  uri      요청을 보낼 경로
- * @param {array}   params   post body에 담을 데이터
+ * @param {string}  path     요청을 보낼 경로
+ * @param {array}   params   post body에 담을 데이터 배열
  * @param {closure} handler  서버로부터 받은 응답을 처리할 클로저
  */
-function sendPostRequest(uri, params, handler) {
+function sendPostRequest(path, params, handler) {
 
   let xhttp = new XMLHttpRequest();
   let body = setParams(params);
 
-  xhttp.open("POST", uri, true);
+  xhttp.open("POST", path, true);
   xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
   xhttp.onreadystatechange = function() {
     if (xhttp.readyState == 4 && xhttp.status == 200) {
@@ -26,7 +26,7 @@ function setParams(values) {
   let params = "";
 
   for (let i = 0; i < values.length; i++) {
-    params += (values[i][0] + "=" + btoa(unescape(encodeURIComponent(values[i][1]))));
+    params += (values[i][0] + "=" + encodeURIComponent(values[i][1]));
     if (i != values.length - 1) {
       params += "&";
     }
