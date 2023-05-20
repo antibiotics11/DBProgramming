68d0c332c7277d8f1414ba6a80a854ac8a660929
ANTIBIOTICS <abx@abx.pe.kr>
2023-05-21 02:26:06 +0900

    UI 스타일 수정
    상단 메뉴에 내 공모전 버튼 추가,
    전체적인 UI 스타일 수정,
    에러페이지 스타일시트를 메인 스타일시트에 통합

--- app/classes/ViewPage/ErrorPage.php
+++ app/classes/ViewPage/ErrorPage.php
@@ -14,8 +14,7 @@ class ErrorPage {
    * @return String 생성된 Container
    */
   public static function loadContainer(int $code, String $message, String $details = ""): String {
-    return sprintf("<div id = \"container\">%s\r\n%s</div>",
-      "<link rel=\"stylesheet\" href=\"/assets/error_style.css\">",
+    return sprintf("<div id = \"container\">%s</div>",
       sprintf("<div id = \"status\">%s<br /><br />%s</div>",
         sprintf("<div id = \"status-message\">%d %s</div>", $code, $message),
         sprintf("<div id = \"status-details\">%s</div>", $details)
