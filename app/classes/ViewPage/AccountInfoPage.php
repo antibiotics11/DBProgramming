4ddf9ed05ad81489e4e16af9c81138be96ba8a16
ANTIBIOTICS <abx@abx.pe.kr>
2023-06-02 16:41:54 +0900

    계정정보 평가가 더 자세히 나오게 수정

--- app/classes/ViewPage/AccountInfoPage.php
+++ app/classes/ViewPage/AccountInfoPage.php
@@ -5,9 +5,33 @@ use \ContestApp\Model\AccountAttribute;
 
 class AccountInfoPage {
 
+  private static function loadRatingDetails(int $ratingPoint): Array {
+
+    $ratingMessage = "평가 없음";
+    $ratingColor = "grey";
+
+    if ($ratingPoint <= -2) {
+      $ratingMessage = "평가 나쁨";
+      $ratingColor = "orange";
+    } else if ($ratingPoint >= 0 && $ratingPoint <= 5) {
+      $ratingMessage = "평가 보통";
+      $ratingColor = "green";
+    } else if ($ratingPoint > 5) {
+      $ratingMessage = "평가 좋음";
+      $ratingColor = "blue";
+    }
+    $ratingMessage = sprintf("%s (%d점)", $ratingMessage, $ratingPoint);
+
+    return [ "message" => $ratingMessage, "color" => $ratingColor ];
+
+  }
+
 	// 계정 정보 페이지
   private const VIEW_INFO_PATH = \APP_VIEW_PATH . "/show_account.html";
   public static function page(Array $accountInfo, Array $collegesList, Array $majorsList): String {
+
+    $ratingInfo = self::loadRatingDetails($accountInfo[AccountAttribute::Rating->value]);
+
     return ViewPage::assemble(
       sprintf(ViewPage::read(self::VIEW_INFO_PATH),
         $accountInfo[AccountAttribute::Phone->value],
@@ -19,10 +43,7 @@ class AccountInfoPage {
         $accountInfo[AccountAttribute::Email->value],
         ViewPage::loadSelectedOption((int)$accountInfo[AccountAttribute::Major->value], $majorsList),
         $accountInfo[AccountAttribute::Birthday->value],
-        (
-          ($accountInfo[AccountAttribute::Rating->value] == -1) ?
-          "점수 없음" : sprintf("%s점", $accountInfo[AccountAttribute::Rating->value])
-        )
+        $ratingInfo["color"], $ratingInfo["message"]
       )
     );
   }
